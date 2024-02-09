e4e8275c003b089b4ee6d98cde7b186cb05fb1a2
MeenYossakorn <143907645+MeenYossakorn@users.noreply.github.com>
2024-02-09 14:06:52 +0700

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:onboarding_screen/screen/introduction_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+bool show = true; //ประกาศแบบ global
+
+void main() async {
+  //เพิ่ม async
+  WidgetsFlutterBinding.ensureInitialized(); //สร้างจนแน่ใจว่าเสร็จแล้ว
+  final prefs = await SharedPreferences.getInstance();
+  show = prefs.getBool('ON_BOARDING') ??
+      true; //Onboarding เป็นชื่อตัวแปล คือ key ที่ยังไม่ได้มัีการใส่ค่าแต่อ้างถึง
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter on Boarding',
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+      ),
+      home: IntroScreen(),
+    );
+  }
+}
